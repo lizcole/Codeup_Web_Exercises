9c9f270dc5fbd0d57fa59d1cc41ea250f32f29b7
Liz Cole <liz.cole1227@gmail.com>
2014-10-14 10:36:47 -0500

    added html practices along with from set up

---
+++ public/my_first_form.php
@@ -0,0 +1,44 @@
+<?php
+
+var_dump($_GET);
+var_dump($_POST);
+
+?>
+
+<html>
+<head>
+	<title>My First Form</title>
+</head>
+<body>
+	<form method="POST" action="/my_first_form.php">
+	<p>	
+        <label for="username">Username</label>
+        <input id="username" name="username" type="text">
+    </p>
+    <p>
+        <label for="password">Password</label>
+        <input id="password" name="password" type="password">
+    </p>
+    <p>
+        <input type="submit">
+    </p>
+</form>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
