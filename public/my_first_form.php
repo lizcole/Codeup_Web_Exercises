682275b1925e5c66b1a187b271e0f7a81643019c
Liz Cole <liz.cole1227@gmail.com>
2014-10-14 11:47:31 -0500

    added html practices for forms and textarea

--- public/my_first_form.php
+++ public/my_first_form.php
@@ -10,19 +10,38 @@ var_dump($_POST);
 	<title>My First Form</title>
 </head>
 <body>
+	<h1>Username and Password Please:</h1>
 	<form method="POST" action="/my_first_form.php">
-	<p>	
-        <label for="username">Username</label>
-        <input id="username" name="username" type="text">
-    </p>
-    <p>
+		<p>	
+       	 <label for="username">Username</label>
+       	 <input id="username" name="username" type="text">
+    	</p>
+    	<p>
         <label for="password">Password</label>
         <input id="password" name="password" type="password">
-    </p>
-    <p>
+    	</p>
+    	<p>
         <input type="submit">
-    </p>
-</form>
+    	</p>
+	</form>
+	
+	<h2>Lets Write an Email:</h2>
+	<form method="GET" action="my_first_form.php">
+		<p>
+			<label for="To">To</label>
+			<input id="To" name="To" type="text">
+		</p>
+		<p>
+			<label for="From">From</label>
+			<input id="From" name="From" type="text">
+		</p>
+		<p>
+			<label for="compose_email">Message</label>
+			<textarea id="compose_email" name="compose_email"></textarea> 
+		</p>
+		<p>
+			<button type="submit">Send</button>
+		</p>
 
 </body>
 </html>
